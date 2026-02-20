7d939cca8009850bac4a0dde1ba388a76a17d95f
gunwoo0614 <parkgun9134@gmail.com>
2026-02-20 09:54:41 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include "plus.h"
-
 int main(void)
 {
-    
-    int a = 3
+    int a = 3;
     int b = 5;
     int c = plus(a, b);
-
     printf("%d\n", c);
     return 0;
 }
+
 
 
 
